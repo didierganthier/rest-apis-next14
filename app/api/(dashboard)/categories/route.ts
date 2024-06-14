a69cd0292918a9d29702234a99a0c8153ee3a6e4
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-06-14 12:29:17 -0400

    Create POST routes

--- app/api/(dashboard)/categories/route.ts
+++ app/api/(dashboard)/categories/route.ts
@@ -32,3 +32,32 @@ export const GET = async (req: Request) => {
         return new NextResponse("Error fetching categories" + error.message, { status: 500 }); 
     }
 }
+
+export const POST = async (req: Request) => {
+    try {
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get("userId");
+
+        const { title } = await req.json();
+
+        if(!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid user ID" }), { status: 400 });
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return new NextResponse(JSON.stringify({ message: "User not found" }), { status: 404 });
+        }
+
+        const newCategory = new Category({ title, user: new Types.ObjectId(userId) });
+
+        await newCategory.save();
+
+        return new NextResponse(JSON.stringify({ message: "Category is created", category: newCategory }), { status: 200 });
+    } catch (error: any) {
+        return new NextResponse("Error creating category" + error.message, { status: 500 });
+    }
+}
