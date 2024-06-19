7cc933adc1eb8121cb457d7519c12658368009d5
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-06-19 12:21:54 -0400

    Create PATCH (update) route for single blog

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -53,3 +53,51 @@ export const GET = async (req: Request, context: {params: any}) => {
         return new NextResponse("Error fetching blog" + error.message, { status: 500 }); 
     }
 }
+
+export const PATCH = async (req: Request, context: {params: any}) => {
+    const blogId = context.params.blog;
+
+    try {
+        const body = await req.json();
+        const { title, description } = body;
+
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get("userId");
+
+        if(!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid user ID" }), { status: 400 });
+        }
+
+
+        if(!blogId || !Types.ObjectId.isValid(blogId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid blog ID" }), { status: 400 });
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return new NextResponse(JSON.stringify({ message: "User not found" }), { status: 404 });
+        }
+
+
+        const blog = await Blog.findOne(
+            { _id: blogId, user: userId }
+        )
+
+        if(!blog) {
+            return new NextResponse(JSON.stringify({ message: "Blog not found" }), { status: 404 });
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            blogId,
+            { title, description },
+            { new: true }
+    );
+
+        return new NextResponse(JSON.stringify({ message: "Blog is updated", blog: updatedBlog }), { status: 200 });
+    } catch (error: any) {
+        return new NextResponse("Error updating blog" + error.message, { status: 500 });
+    }
+}
